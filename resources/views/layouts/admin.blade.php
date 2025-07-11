fbad7590222d0d279e31c7877dec565c152b40f0
Tamara <atanasova_tamara@yahoo.com>
2025-07-11 23:28:26 +0200

    insert

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -25,12 +25,10 @@
         <aside class="w-64 bg-white border-r h-screen fixed top-16 left-0 p-4 space-y-2">
             <a href="{{ route('items.create') }}" class="block px-4 py-2 rounded hover:bg-gray-200">Add Item</a>
             @foreach ($subtypes as $subtype)
-                <li class="mr-2">
                     <a href="#subtype-{{ $subtype->id }}" 
-                       class="inline-block p-4 rounded-t-lg hover:text-blue-600 hover:border-b-2 border-blue-600">
+                       class="block px-4 py-2 rounded hover:bg-gray-200">
                        {{ $subtype->name }}
                     </a>
-                </li>
             @endforeach
         </aside>
 
